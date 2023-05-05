espositoandrea/chatidea

commit c38ac78077e729571da20ec9dc58a5b78630c2d2

Introduce connection string builder
Introduced a simple builder class for creating connection strings. This
allows to ensure consistent formatting of the connection string, and
allows also for a leaner code as the "conditional" adding of components
to the string is hidden inside a class. Finally, this also ensure that
when adding a component to the string, the component is terminated with
a semicolon.

Known issue: multiple calls to the same method will add the same
component multiple times. This needs to be fixed, but it's fine for now.

modules/database/broker.py
@@ -34,13 +34,52 @@ def test_connection():
     logger.info('Connection closed.')
 
 
+class ConnectionStringBuilder:
+    def __add_to_str(self, key: str, val: Any):
+        # FIXME: multiple calls with the same key produce repetitions in the
+        #        final string.
+        self.string += f"{key.upper()}={val};"
+
+    def __init__(self, driver: str, server: str, database: str,
+                 port: Optional[typing.Union[str, int]] = None):
+        self.string = ""
+        self.__add_to_str("driver", driver)
+        self.__add_to_str("server", server)
+        self.__add_to_str("database", database)
+        if port:
+            self.__add_to_str("port", port)
+
+    def login(self, username: str,
+              password: Optional[str] = None) -> "ConnectionStringBuilder":
+        self.__add_to_str("UID", username)
+        if password:
+            self.__add_to_str("PWD", password)
+        return self
+
+    def authentication(self, auth: Optional[str]) -> "ConnectionStringBuilder":
+        if auth:
+            self.__add_to_str("authentication", auth)
+        return self
+
+    def charset(self, charset: Optional[str]) -> "ConnectionStringBuilder":
+        if charset:
+            self.__add_to_str("charset", charset)
+        return self
+
+    def __str__(self):
+        return self.string
+
+    def get_str(self) -> str:
+        return self.string
+
+
 def _connect() -> pyodbc.Connection:
-    connection = pyodbc.connect(f'DRIVER={DB_DRIVER};'
-                                f'SERVER={DB_HOST};'
-                                f'DATABASE={DB_NAME};'
-                                f'UID={DB_USER};'
-                                f'PWD={DB_PASSWORD};'
-                                f'CHARSET={DB_CHARSET};')
+    connection_str = (ConnectionStringBuilder(DB_DRIVER, DB_HOST, DB_NAME)
+                      .login(DB_USER, DB_PASSWORD)
+                      .charset(DB_CHARSET)
+                      .authentication("SqlPassword"))
+    logger.debug("Connection string: %s", connection_str)
+    connection = pyodbc.connect(connection_str.get_str())
 
     # MySQL-specific options for encoding issues
     connection.setdecoding(pyodbc.SQL_CHAR, encoding='utf-8')

commit ac96f31afe6c10fd90a68a62480bc40a39d12c40

Prevent repeating parameters in connection string
Prevent parameters from being repeated in a connection string when the
same method of the Connection String Builder class is called multiple
times. The new behavior is to replace the older value with the newest.

Fixes issue introduced in c38ac78077e729571da20ec9dc58a5b78630c2d2

modules/database/broker.py
@@ -38,11 +38,11 @@ class ConnectionStringBuilder:
     def __add_to_str(self, key: str, val: Any):
         # FIXME: multiple calls with the same key produce repetitions in the
         #        final string.
-        self.string += f"{key.upper()}={val};"
+        self.parameters[key.upper()] = val
 
     def __init__(self, driver: str, server: str, database: str,
                  port: Optional[typing.Union[str, int]] = None):
-        self.string = ""
+        self.parameters = {}
         self.__add_to_str("driver", driver)
         self.__add_to_str("server", server)
         self.__add_to_str("database", database)
@@ -67,10 +67,10 @@ class ConnectionStringBuilder:
         return self
 
     def __str__(self):
-        return self.string
+        return ";".join(f"{k}={v}" for k, v in self.parameters.items())
 
     def get_str(self) -> str:
-        return self.string
+        return str(self)
 
 
 def _connect() -> pyodbc.Connection: