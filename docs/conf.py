550f9f3856ce5b9673bb45c1f2e16e0c7d2cb4e2
Andrea Esposito <esposito_andrea99@hotmail.com>
2023-05-31 14:22:52 +0200

    Set PDF paper size to A4

--- docs/conf.py
+++ docs/conf.py
@@ -22,10 +22,14 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# -- Options for LaTeX output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-latex-output
+latex_elements = {
+    'papersize': 'a4paper'
+}
