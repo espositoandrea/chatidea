823bd5d950fead1150c076ad26a8ea703bf40e9b
Andrea Esposito <esposito_andrea99@hotmail.com>
2023-07-03 13:06:21 +0200

    Add logging to action execution

--- chatidea/actions/common.py
+++ chatidea/actions/common.py
@@ -1,4 +1,5 @@
 import functools
+import logging
 from typing import Callable
 
 from chatidea import extractor
@@ -10,16 +11,17 @@ ActionReturn = tuple[list[str], list[Button]]
 ActionFunction = Callable[[list[extractor.Entity], Context], ActionReturn]
 Action = Callable[[list[extractor.Entity], Context], Response]
 
+logger = logging.getLogger(__name__)
+
 
 def action(func: ActionFunction):
     @functools.wraps(func)
     def wrapper(*args, **kwargs) -> Response:
         response = Response()
+        logger.info("Calling function: %s", func.__name__)
+        logger.debug("Parameters: %s", str((*args, kwargs)))
         message, buttons = func(*args, **kwargs)
-        if isinstance(message, str):
-            response.add_message(message)
-        else:
-            response.add_messages(message)
+        response.add_messages(message)
         response.add_buttons(buttons)
         return response
 
